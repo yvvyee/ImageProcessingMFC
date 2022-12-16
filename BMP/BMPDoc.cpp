8925d808f06f7353591384058965a5e3128ffe7d
Lee, Young-Woon <yw.lee@ivpl.sookmyung.ac.kr>
2022-12-16 12:37:27 +0900

    ME 알고리즘 주석 정리

--- BMP/BMPDoc.cpp
+++ BMP/BMPDoc.cpp
@@ -229,13 +229,14 @@ CDib *CBMPDoc::OnColormodelRgb2yuv()
 
 void CBMPDoc::OnFrameprocessingMotionestimation()
 {
+	// ref 는 현재 열린 프레임
 	CDib& ref = this->m_Dib;
 
 	register int i, j, k, l, m, n;
 	int w = ref.GetWidth();
 	int h = ref.GetHeight();
 
-	// 1. 첫번째 프레임은 열려있는 상태, 두번째 프레임을 열고 화면에 출력한다.
+	// cur 는 2번째로 여는 프레임
 	if (ref.GetBitCount() == 24)
 	{
 		CDib cur;
@@ -252,86 +253,91 @@ void CBMPDoc::OnFrameprocessingMotionestimation()
 		}
 		AfxNewImage(cur);
 
-		// 2. MacroBlock (MB), SearchRange (SR), Residual 버퍼를 생성한다.
-		int mb_sz = 16;
-		int margin = 7;
-		int sr_sz = margin * 2 + mb_sz;
-		DOUBLE** macro_block	= Alloc2DMem(mb_sz, mb_sz);
-		DOUBLE** search_range	= Alloc2DMem(sr_sz, sr_sz);
-		DOUBLE** residual		= Alloc2DMem(h, w);
+		int mb_sz = 16;										// 매크로블록(MB) 의 크기는 16 (정사각형)
+		int margin = 7;										// margin = 7, MB 의 상하좌우로 커지는 크기
+		int sr_sz = margin * 2 + mb_sz;						// 탐색영역(SR) 의 크기는 30 (정사각형)
+		DOUBLE** macro_block	= Alloc2DMem(mb_sz, mb_sz);	// MB 버퍼 동적할당
+		DOUBLE** search_range	= Alloc2DMem(sr_sz, sr_sz);	// SR 버퍼 동적할당
+		DOUBLE** residual		= Alloc2DMem(h, w);			// residual 버퍼 동적할당
 		
-		// 3. RGB 포맷의 두 프레임을 YUV 포맷으로 변환한다. 
+		// 2개의 프레임을 YUV 포맷으로 변환
 		DOUBLE*** refYUV = RGB2YUV(ref);
 		DOUBLE*** curYUV = RGB2YUV(cur);
 		
-		// ReferenceFrame 의 Y 값을 마진 버퍼에 복사
+		// 프레임의 크기 (w * h) 가 상하좌우 margin 만큼 큰 버퍼를 동적할당
 		DOUBLE** refLumaWithMargin = Alloc2DMem(h + margin * 2, w + margin * 2);
-		for (i = 0; i < h; i++)
-		{
-			for (j = 0; j < w; j++)
-			{
+
+		// margin 크기만큼 키운 버퍼에 ref 의 Y 채널 값을 복사 (가운데 위치해야함)
+		for (i = 0; i < h; i++) {
+			for (j = 0; j < w; j++) {
 				refLumaWithMargin[i + margin][j + margin] = refYUV[0][i][j];
 			}
 		}
 			
-		// 4. Motion Estimation 처리 시작
-		for (i = 0; i < h - mb_sz; i += mb_sz) // 프레임의 해상도 h, w 에 대응하는 2중 루프부터 시작한다.
+		// Motion Estimation 시작: 프레임의 크기 (h, w) 만큼 반복문 순회 
+		// (MB 가 서로 겹치지 않게 주의)
+		// (MB 크기를 고려하여 반복문 종료 시점 설정해야 함)
+		for (i = 0; i < h - mb_sz; i += mb_sz)
 		{
 			for (j = 0; j < w - mb_sz; j += mb_sz)
 			{
-				for (k = 0; k < sr_sz; k++) // SR 의 크기에 대응하는 2중 루프
-				{
-					for (l = 0; l < sr_sz; l++)
-					{
-						// 첫번째 프레임 = 참조 프레임 = ReferenceFrame 에서 탐색 영역 = SearchRange 값을 복사한다.
+				// margin 이 포함된 ref 의 Y 버퍼에서 SR 을 복사
+				for (k = 0; k < sr_sz; k++) {
+					for (l = 0; l < sr_sz; l++) {
 						search_range[k][l] = refLumaWithMargin[i + k][j + l];
 					}
 				}
 
-				for (k = 0; k < mb_sz; k++) // MB 의 크기에 대응하는 2중 루프
-				{
-					for (l = 0; l < mb_sz; l++)
-					{
-						// 두번째 프레임 = 현재 프레임 = CurrentFrame 에서 매크로 블록 = MacroBlock 값을 복사한다.
+				// cur 의 Y 버퍼에서 MB 을 복사
+				for (k = 0; k < mb_sz; k++) {
+					for (l = 0; l < mb_sz; l++) {
 						macro_block[k][l] = curYUV[0][i + k][j + l];
 					}
 				}
 					
-				// 먼저, 가장 작은 sad 값, 위치 좌표를 기억할 변수들 선언
+				// SR에서 오차가 가장 적은 위치의 가로, 세로 좌표를 기억할 변수 선언
 				int best_k = 0;
 				int best_l = 0;
+
+				// 최적의 SAD 값을 저장할 변수 선언, 초기값은 충분히 큰 값이어야 함
 				DOUBLE best_sad = DBL_MAX;
+
+
 				DOUBLE rv = 0.;
 				DOUBLE cv = 0.;
-				for (k = 0; k < sr_sz - mb_sz; k++) // SR 크기에 대응하는 2중 루프
-				{
-					for (l = 0; l < sr_sz - mb_sz; l++)
-					{
+
+				// SR 크기 만큼 반복문 순회
+				for (k = 0; k < sr_sz - mb_sz; k++) {
+					for (l = 0; l < sr_sz - mb_sz; l++) {
+
 						DOUBLE sad = 0.;
-						for (m = 0; m < mb_sz; m++) // MB 크기에 대응하는 2중 루프
-						{
-							for (n = 0; n < mb_sz; n++)
-							{
+
+						// MB 크기 만큼 반복문 순회
+						for (m = 0; m < mb_sz; m++)	{
+							for (n = 0; n < mb_sz; n++) {
+
+								// SAD: Sum of Absolute Differece 계산
 								rv = search_range[k + m][l + n];
 								cv = macro_block[m][n];
-								sad += abs(rv - cv); // SAD: Sum of Absolute Differece
 							}
 						}
-						// SAD 가 최소인지 확인하고 정보를 저장한다.
-						if (best_sad > sad)
-						{
+
+						// SAD 가 최소인지 확인하고 최적 SAD 값 갱신
+						if (best_sad > sad) {
 							best_sad = sad;
+
+							// 최적 SAD 값을 갖는 SR 의 위치 좌표 갱신
 							best_k = k;
 							best_l = l;
 						}
 					}
 				}
-					
-				for (m = 0; m < mb_sz; m++) // MB 크기에 대응하는 2중 루프
-				{
-					for (n = 0; n < mb_sz; n++)
-					{
-						// SAD 가 최소가 되는 최적 위치와 매크로 블록 사이의 잔차 = Residual (Absolute Difference) 을 구한다.
+				
+				// MB 크기만큼 반복문 순회
+				for (m = 0; m < mb_sz; m++)  {
+					for (n = 0; n < mb_sz; n++) {
+						
+						// SAD 최적인 SR 과 MB 간에 Absolute Difference 를 구해 residual 에 저장
 						rv = search_range[best_k + m][best_l + n];
 						cv = macro_block[m][n];
 						residual[i + m][j + n] = abs(rv - cv);
@@ -341,23 +347,22 @@ void CBMPDoc::OnFrameprocessingMotionestimation()
 		}
 		// Motion Estimation 종료
 		
-		// 5. 실수 범위의 잔차 = Residual 을 정수 범위로 스케일링하고 ReferenceFrame 의 RGB 버퍼에 복사한다.
+		// 실수 범위의 residual 을 정수 범위로 스케일링하고 ref 의 RGB 버퍼에 복사
 		CDib cur_res;
 		cur_res.Copy(&cur);
-		
 		RGBBYTE** ptr = cur_res.GetRGBPtr();
-		for (i = 0; i < h; i++)
-		{
-			for (j = 0; j < w; j++)
-			{
+		for (i = 0; i < h; i++) {
+			for (j = 0; j < w; j++) {
 				ptr[i][j].r = (BYTE)(residual[i][j] * 255.);
 				ptr[i][j].g = (BYTE)(residual[i][j] * 255.);
 				ptr[i][j].b = (BYTE)(residual[i][j] * 255.);
 			}
 		}
+
+		// 화면에 표시
 		AfxNewImage(cur_res);
 
-		// 동적할당한 모든 버퍼를 해제한다.
+		// 동적할당한 모든 버퍼를 해제
 		delete[] macro_block;
 		delete[] search_range;
 		delete[] residual;
